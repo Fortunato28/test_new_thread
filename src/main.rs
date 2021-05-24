79aae5281ba32a61a02ce905ce1a7b1f782cd6d1
Anton Sapunov <fort.sav.28@gmail.com>
2021-05-24 17:52:50 +0700

    Trying to run in local set

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,69 @@
-use actix_web::{get, rt, App, HttpResponse, HttpServer, Responder};
-use anyhow::Result;
+use actix_web::{dev::Server, get, rt, App, HttpResponse, HttpServer, Responder};
+use anyhow::{anyhow, Result};
+use parking_lot::Mutex;
 use std::{sync::Arc, thread, time};
-use tokio::runtime::Handle;
+use tokio::{sync::oneshot, task};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let panel = ControlPanel::new("127.0.0.1:8080");
-    panel.start()?;
+    panel.clone().start()?;
 
     println!("WAITING 10 SECONDS");
     thread::sleep(time::Duration::from_secs(10));
+
+    let mut rx = panel.graceful_shutdown().ok_or(anyhow!("No receiver"))?;
+    let _ = rx.try_recv();
 
     Ok(())
 }
 
 struct ControlPanel {
     address: String,
+    work_finished_receiver: Mutex<Option<oneshot::Receiver<Result<()>>>>,
+    server: Arc<Mutex<Option<Server>>>,
 }
 
 impl ControlPanel {
     fn new(address: &str) -> Arc<Self> {
         Arc::new(Self {
             address: address.to_owned(),
+            work_finished_receiver: Mutex::new(None),
+            server: Arc::new(Mutex::new(None)),
         })
     }
 
     fn start(self: Arc<Self>) -> std::io::Result<()> {
-        let handle = Handle::current();
         thread::spawn(move || {
-            let _ = handle.enter();
-
             let _ = self.start_server();
         });
 
         Ok(())
     }
 
-    fn start_server(&self) -> std::io::Result<()> {
-        let system = rt::System::new();
-        let local = tokio::task::LocalSet::new();
+    async fn stop(self: Arc<Self>) {
+        let (tx, rx) = oneshot::channel();
+        (*self.work_finished_receiver.lock()) = Some(rx);
 
+        let maybe_server = self.server.lock();
+        if let Some(server) = &(*maybe_server) {
+            server.stop(true).await;
+            let _ = tx.send(Ok(()));
+        }
+    }
+
+    fn start_server(self: Arc<Self>) -> std::io::Result<()> {
         let address = self.address.clone();
 
-        local.spawn_local(async move {
-            HttpServer::new(|| App::new().service(health))
-                .bind(&address)
-                .expect("fix it")
-                .shutdown_timeout(3)
-                .workers(1)
-                .run()
-        });
+        let system = Arc::new(rt::System::new());
+        let server = HttpServer::new(|| App::new().service(health))
+            .bind(&address)?
+            .shutdown_timeout(3)
+            .workers(1);
 
-        system.block_on(local);
+        system.block_on(async {
+            *self.server.lock() = Some(server.run());
+        });
 
         Ok(())
     }
@@ -60,4 +72,36 @@ impl ControlPanel {
 #[get("/health")]
 async fn health() -> impl Responder {
     HttpResponse::Ok().body("Server is working")
+}
+
+pub trait Service: Send + Sync + 'static {
+    fn name(&self) -> &str;
+
+    fn graceful_shutdown(self: Arc<Self>) -> Option<oneshot::Receiver<Result<()>>>;
+}
+
+impl Service for ControlPanel {
+    fn name(&self) -> &str {
+        "ControlPanel"
+    }
+
+    fn graceful_shutdown(self: Arc<Self>) -> Option<oneshot::Receiver<Result<()>>> {
+        let cloned_self = self.clone();
+
+        let local = task::LocalSet::new();
+        task::spawn_local(async move { cloned_self.stop() });
+
+        let work_finished_receiver = self.work_finished_receiver.lock().take();
+        if work_finished_receiver.is_none() {
+            dbg!(
+                "'work_finished_receiver' wasn't created when started graceful shutdown in {}",
+                self.name()
+            );
+        }
+
+        //tokio::spawn(self.stop());
+        //self.stop();
+
+        work_finished_receiver
+    }
 }
